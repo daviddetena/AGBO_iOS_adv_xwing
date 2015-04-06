f25cab1177ff83beaa8ff0c8f89b7044ecb59736
David de Tena <daviddetenacalvo@gmail.com>
2015-04-06 23:32:46 +0200

    Initial Commit

---
+++ Xwing/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Xwing
+//
+//  Created by David de Tena on 06/04/15.
+//  Copyright (c) 2015 David de Tena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
